d9e531e285989fd22625ebb064f1ceee4d98e214
dev-b4 <199273268+dev-b4@users.noreply.github.com>
2026-09-03 18:20:11 -0300

    chore: remove scripts de manutencao temporarios

--- debug_crash.js
+++
@@ -1,7 +0,0 @@
-import { getEvidences, generateAcreditationScore } from './src/api/mockData.js';
-try {
-  const score = generateAcreditationScore(0, '0xc88d4860d4ddb7a7621b4a919360b4775d93a5ef');
-  console.log("Score:", score);
-} catch (e) {
-  console.error("Crash:", e);
-}
